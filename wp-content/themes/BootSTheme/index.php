e5c0b6c5b9b8c97e50bb1fc6539e33cd3e7ef4a4
Reybel C <Reybel24@gmail.com>
2019-03-14 19:18:32 -0400

    add latest post feature, add author to post

--- wp-content/themes/BootSTheme/index.php
+++ wp-content/themes/BootSTheme/index.php
@@ -3,9 +3,31 @@
 
 <div class = "container">
 
-    <div class = "jumbotron">
-        <h2>Post Title</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu erat, congue eget ornare et, luctus eget purus. Etiam et risus metus. Nam sed mi tellus. Mauris molestie massa eu metus tempor, in hendrerit arcu adipiscing.</p>
+    <?php
+    query_posts('posts_per_page=1');
+    while(have_posts()) : the_post(); ?>
+        <div>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <p><?php the_excerpt(); ?></p>
+        </div>
+    <?php endwhile; wp_reset_query(); ?>
+
+    <div class="panel panel-default panel-body">
+        <div>
+            <div>
+                <ul>
+                    <?php wp_list_categories('orderby=name&title_li='); ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <?php while(have_posts()) : the_post(); ?>
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <p><?php the_excerpt(); ?></p>
+        <p> posted by <?php the_author(); ?>
+            <?php endwhile; wp_reset_query(); ?>
     </div>
 
     <div class = "panel panel-default panel-body">
