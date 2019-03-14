2bd5733801b0326deffd7bc0875190f32c0c03a0
Reybel C <Reybel24@gmail.com>
2019-03-14 16:04:28 -0400

    Create basic html template for theme

--- wp-content/themes/BootSTheme/header.php
+++ wp-content/themes/BootSTheme/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Bootstrap WordPress Theme</title>
+    <title><?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
@@ -16,3 +16,8 @@
             <span class = "icon-bar"></span>
             <span class = "icon-bar"></span>
         </button>
+
+        <php
+            echo get_stylesheet_uri();
+            wp_head();
+        ?>
